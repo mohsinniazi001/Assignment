be8bdc3bdcfe51b2a06d1ed8ee17376c1af81b1b
Mohsin Farooq Khan <mohsin.mfk1122@gmail.com>
2022-08-15 00:17:20 +0500

    added db in firebase file and done home page work

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { signOut, onAuthStateChanged } from "firebase/auth"
-import { auth } from "../Firebase/FirebaseConfig"
+import { auth, db } from "../Firebase/FirebaseConfig"
 import { useNavigate } from "react-router-dom";
-
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../Firebase/FirebaseConfig'
 export default function Home() {
     let navigate = useNavigate();
     const [lemail, setlemail] = useState({});
+    const [input, setInput] = useState('');
     useEffect(() => {
         onAuthStateChanged(auth, (currentUser) => {
             setlemail(currentUser)
@@ -17,9 +19,31 @@ export default function Home() {
             navigate("/");
         }
     }
+    const createTodo = async (e) => {
+        e.preventDefault(e);
+        if (input == '') {
+            alert('Please enter a valid todo');
+            return;
+        }
+        await addDoc(collection(db, 'tasks'), {
+            Name: input,
+        });
+        setInput('');
+    };
 
     return (
         <div>
+            <form>
+                <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                type='text'
+                placeholder='Add Todo'
+                />
+                <button>
+                    Submit
+                </button>
+            </form>
             <button onClick={() => Logout()}>Logout</button>
             <h2>You Have Login With {lemail.email}</h2>
         </div>
--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
-
+import { getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdh9P8aXzFMxdQ32GBCsKnLb65ZBHrRpE",
@@ -15,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
